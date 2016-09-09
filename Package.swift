a32807ac25d15897f506a5fffa415287209b72d8
Tanner Nelson <me@tanner.xyz>
2016-09-09 03:16:39 -0400

    gm updates

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FluentMySQL",
     dependencies: [
-   		.Package(url: "https://github.com/vapor/mysql.git", majorVersion: 0, minor: 5),
-   		.Package(url: "https://github.com/vapor/fluent.git", majorVersion: 0, minor: 10),
+   		.Package(url: "https://github.com/vapor/mysql.git", majorVersion: 0, minor: 6),
+   		.Package(url: "https://github.com/vapor/fluent.git", majorVersion: 0, minor: 11),
     ]
 )
