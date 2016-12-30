81b031074ed4f6c61abb5a8ab82c0eabef095b73
Casper Rasmussen <cr@nodes.dk>
2016-12-31 00:29:40 +0100

    Changed to unsigned int as id

--- Sources/FluentMySQL/MySQLSerializer.swift
+++ Sources/FluentMySQL/MySQLSerializer.swift
@@ -7,7 +7,7 @@ public final class MySQLSerializer: GeneralSQLSerializer {
     public override func sql(_ type: Schema.Field.DataType) -> String {
         switch type {
         case .id:
-            return "INT(11) PRIMARY KEY AUTO_INCREMENT"
+            return "INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT"
         case .int:
             return "INT(11)"
         case .string(let length):
