4db84625231a222f420a5826cc85b6a662564e2a
Tanner Nelson <me@tanner.xyz>
2017-03-27 17:21:46 +0100

    beta

--- Package.swift
+++ Package.swift
@@ -1,11 +1,13 @@
 import PackageDescription
 
+let beta = Version(2,0,0, prereleaseIdentifiers: ["alpha"])
+
 let package = Package(
     name: "FluentMySQL",
     dependencies: [
     	// Robust MySQL interface for Swift.
-   		.Package(url: "https://github.com/vapor/mysql.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+   		.Package(url: "https://github.com/vapor/mysql.git", beta),
    		// Swift models, relationships, and querying for NoSQL and SQL databases.
-   		.Package(url: "https://github.com/vapor/fluent.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"]))
+   		.Package(url: "https://github.com/vapor/fluent.git", beta)
     ]
 )
