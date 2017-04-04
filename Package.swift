2941ec540944f3652cd50eeab0323373577e76b9
tanner0101 <me@tanner.xyz>
2017-04-04 16:00:38 +0200

    fix foreign / index issues

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 import PackageDescription
 
-let beta = Version(2,0,0, prereleaseIdentifiers: ["alpha"])
+let beta = Version(2,0,0, prereleaseIdentifiers: ["beta"])
 
 let package = Package(
     name: "MySQLDriver",
@@ -8,6 +8,8 @@ let package = Package(
     	// Robust MySQL interface for Swift.
    		.Package(url: "https://github.com/vapor/mysql.git", beta),
    		// Swift models, relationships, and querying for NoSQL and SQL databases.
-   		.Package(url: "https://github.com/vapor/fluent.git", beta)
+   		.Package(url: "https://github.com/vapor/fluent.git", beta),
+   		// Random number generation
+   		.Package(url: "https://github.com/vapor/random.git", Version(1,0,0, prereleaseIdentifiers: ["beta"]))
     ]
 )
--- Sources/MySQLDriver/ForeignKeys.swift
+++ Sources/MySQLDriver/ForeignKeys.swift
@@ -1,18 +1,28 @@
 import Fluent
+import Random
 
 extension Builder {
     /// Adds an index to the column.
     public func index(_ column: String) {
-        raw("INDEX _fluent_index_\(column) (`\(column)`)")
+        raw("INDEX `_fluent_index_\(random)` (`\(column)`)")
     }
-
+    
     /// Adds a foreign key constraint from a local
     /// column to a column on the foreign entity.
     public func foreignKey<E: Entity>(
         _ localColumn: String,
         references foreignColumn: String,
         on entity: E.Type = E.self
-    ) {
-        raw("CONSTRAINT _fluent_foreignkey_\(localColumn)_\(foreignColumn) FOREIGN KEY (`\(localColumn)`) REFERENCES `\(E.entity)` (`\(foreignColumn)`)")
+        ) {
+        raw("CONSTRAINT `_fluent_foreignkey_\(random)` FOREIGN KEY (`\(localColumn)`) REFERENCES `\(E.entity)` (`\(foreignColumn)`)")
+    }
+}
+
+extension Builder {
+    fileprivate var random: String {
+        return OSRandom()
+            .bytes(count: 3)
+            .hexEncoded
+            .makeString()
     }
 }
