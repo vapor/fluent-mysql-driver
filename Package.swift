deb30e7bbcbfce6f90c151bd070fcb0fb0770445
Tanner Nelson <me@tanner.xyz>
2017-12-19 09:40:53 -0500

    mysql driver

--- Package.swift
+++ Package.swift
@@ -1,13 +1,17 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "MySQLDriver",
+    name: "FluentMySQL",
+    products: [
+        .library(name: "FluentMySQL", targets: ["FluentMySQL"]),
+    ],
     dependencies: [
-    	// Robust MySQL interface for Swift.
-   		.Package(url: "https://github.com/vapor/mysql.git", majorVersion: 2),
-   		// Swift models, relationships, and querying for NoSQL and SQL databases.
-   		.Package(url: "https://github.com/vapor/fluent.git", majorVersion: 2),
-   		// Random number generation
-   		.Package(url: "https://github.com/vapor/random.git", majorVersion: 1),
+        // Cryptography modules
+        .package(url: "https://github.com/vapor/mysql.git", .branch("beta")),
+    ],
+    targets: [
+        .target(name: "FluentMySQL", dependencies: ["Fluent", "FluentSQL", "MySQL"]),
+        .testTarget(name: "FluentMySQLTests", dependencies: ["FluentMySQL"]),
     ]
 )
