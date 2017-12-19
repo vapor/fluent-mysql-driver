7f6b8cf4a22aaa8b17befb66b714dc6d380cd614
Tanner Nelson <me@tanner.xyz>
2017-12-19 16:40:09 -0500

    fluentbenchmark fix

--- Package.swift
+++ Package.swift
@@ -7,11 +7,13 @@ let package = Package(
         .library(name: "FluentMySQL", targets: ["FluentMySQL"]),
     ],
     dependencies: [
+        // Swift ORM (queries, models, and relations) for NoSQL and SQL databases.
         .package(url: "https://github.com/vapor/fluent.git", .branch("beta")),
+        // Robust MySQL interface for Swift
         .package(url: "https://github.com/vapor/mysql.git", .branch("beta")),
     ],
     targets: [
         .target(name: "FluentMySQL", dependencies: ["Fluent", "FluentSQL", "MySQL"]),
-        .testTarget(name: "FluentMySQLTests", dependencies: ["FluentMySQL"]),
+        .testTarget(name: "FluentMySQLTests", dependencies: ["FluentBenchmark", "FluentMySQL"]),
     ]
 )
