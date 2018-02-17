8c5b5dc37d4ab1e86787cb5091e5b0e9d57308f1
Joannis Orlandos <joannis@orlandos.nl>
2018-02-17 14:04:28 +0100

    update dependency versions

--- Package.swift
+++ Package.swift
@@ -8,9 +8,9 @@ let package = Package(
     ],
     dependencies: [
         // Swift ORM (queries, models, and relations) for NoSQL and SQL databases.
-        .package(url: "https://github.com/vapor/fluent.git", .exact("3.0.0-beta.2")),
+        .package(url: "https://github.com/vapor/fluent.git", "3.0.0-beta.2"..<"3.0.0-beta.3"),
         // Robust MySQL interface for Swift
-        .package(url: "https://github.com/vapor/mysql.git", .exact("3.0.0-beta.2")),
+        .package(url: "https://github.com/vapor/mysql.git", "3.0.0-beta.2"..<"3.0.0-beta.3"),
     ],
     targets: [
         .target(name: "FluentMySQL", dependencies: ["Fluent", "FluentSQL", "MySQL"]),
