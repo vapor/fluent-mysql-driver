e941ebbb989a22a9dd15360681d7a511354d4ee7
tanner0101 <me@tanner.xyz>
2018-03-21 18:00:29 -0400

    rc.2 updates

--- Package.swift
+++ Package.swift
@@ -9,13 +9,13 @@ let package = Package(
     dependencies: [
 
         // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
-        .package(url: "https://github.com/vapor/core.git", .branch("master")),
+        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc.2"),
 
         // Swift ORM framework (queries, models, and relations) for building NoSQL and SQL database integrations.
-        .package(url: "https://github.com/vapor/fluent.git", .branch("master")),
+        .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0-rc.2"),
 
         // 🐬 Pure Swift MySQL client built on non-blocking, event-driven sockets.
-        .package(url: "https://github.com/vapor/mysql.git", .branch("master")),
+        .package(url: "https://github.com/vapor/mysql.git", from: "3.0.0-rc.2"),
     ],
     targets: [
         .target(name: "FluentMySQL", dependencies: ["Async", "CodableKit", "Fluent", "FluentSQL", "MySQL"]),
