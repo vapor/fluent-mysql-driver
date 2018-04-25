fe0a4c6bd76387e528f77f4c3e533d6aefe0a21f
tanner0101 <me@tanner.xyz>
2018-04-25 18:17:34 -0400

    dbkit 1.0.0

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
         .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc.2"),
 
         // Swift ORM framework (queries, models, and relations) for building NoSQL and SQL database integrations.
-        .package(url: "https://github.com/vapor/fluent.git", .branch("dbkit-gm")),
+        .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0-rc"),
 
         // 🐬 Pure Swift MySQL client built on non-blocking, event-driven sockets.
-        .package(url: "https://github.com/vapor/mysql.git", .branch("dbkit-gm")),
+        .package(url: "https://github.com/vapor/mysql.git", from: "3.0.0-rc"),
     ],
     targets: [
         .target(name: "FluentMySQL", dependencies: ["Async", "Fluent", "FluentSQL", "MySQL"]),
