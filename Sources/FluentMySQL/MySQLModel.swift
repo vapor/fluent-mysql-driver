2510f9a6c3f2f22233f38bb0fdd86c6d3ab41ed0
tanner0101 <me@tanner.xyz>
2018-06-19 14:33:11 -0400

    implement MySQLUpsert custom support

--- Sources/FluentMySQL/MySQLModel.swift
+++ Sources/FluentMySQL/MySQLModel.swift
@@ -1,8 +1,17 @@
 import Foundation
 
+public protocol _MySQLModel: Model, MySQLTable where Self.Database == MySQLDatabase { }
+
+extension _MySQLModel {
+    /// See `SQLTable`.
+    public static var sqlTableIdentifierString: String {
+        return entity
+    }
+}
+
 /// A MySQL database model.
 /// See `Fluent.Model`.
-public protocol MySQLModel: Model where Self.Database == MySQLDatabase, Self.ID == Int {
+public protocol MySQLModel: _MySQLModel where Self.ID == Int {
     /// This MySQL Model's unique identifier.
     var id: ID? { get set }
 }
@@ -18,7 +27,7 @@ public protocol MySQLPivot: Pivot, MySQLModel { }
 
 /// A MySQL database UUID model.
 /// See `Fluent.Model`.
-public protocol MySQLUUIDModel: Model where Self.Database == MySQLDatabase, Self.ID == UUID {
+public protocol MySQLUUIDModel: _MySQLModel where Self.ID == UUID {
     /// This MySQL Model's unique identifier.
     var id: UUID? { get set }
 }
@@ -32,5 +41,21 @@ extension MySQLUUIDModel {
 /// See `Fluent.Pivot`.
 public protocol MySQLUUIDPivot: Pivot, MySQLUUIDModel { }
 
+
+/// A MySQL database String model.
+/// See `Fluent.Model`.
+public protocol MySQLStringModel: _MySQLModel where Self.ID == String {
+    /// This MySQL Model's unique identifier.
+    var id: String? { get set }
+}
+
+extension MySQLStringModel {
+    /// See `Model.idKey`
+    public static var idKey: IDKey { return \.id }
+}
+
+/// A MySQL database String pivot.
+/// See `Fluent.Pivot`.
+public protocol MySQLStringPivot: Pivot, MySQLStringModel { }
 
 public protocol MySQLMigration: Migration where Self.Database == MySQLDatabase { }
