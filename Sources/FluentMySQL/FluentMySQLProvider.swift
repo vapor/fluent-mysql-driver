2e2f6ea6a97d0b352cf55e6ec289dc1276a6b31a
tanner0101 <me@tanner.xyz>
2018-07-24 19:10:43 -0400

    remove leftover MySQLConfig

--- Sources/FluentMySQL/FluentMySQLProvider.swift
+++ Sources/FluentMySQL/FluentMySQLProvider.swift
@@ -20,29 +20,3 @@ public final class FluentMySQLProvider: Provider {
         return .done(on: worker)
     }
 }
-
-public struct MySQLConfig: Service {
-    /// The hostname to which connections will be connected
-    public let hostname: String
-
-    /// The port to which connections will be connected
-    public let port: UInt16
-
-    /// The username to authenticate with
-    public let user: String
-
-    /// The password to authenticate with
-    public let password: String?
-
-    /// The database to select
-    public let database: String
-
-    /// Creates a new `MySQLConfig`.
-    public init(hostname: String, port: UInt16 = 3306, user: String, password: String?, database: String) {
-        self.hostname = hostname
-        self.port = port
-        self.user = user
-        self.password = password
-        self.database = database
-    }
-}
