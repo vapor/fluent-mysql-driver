7281ba0dd9affa0a2b25fa801498ae6a63e0ada5
Tanner Nelson <me@tanner.xyz>
2019-03-20 17:38:07 -0400

    FluentMySQLDriver 4.0

--- Sources/FluentMySQLDriver/MySQL+Fluent.swift
+++ Sources/FluentMySQLDriver/MySQL+Fluent.swift
@@ -49,27 +49,41 @@ extension ConnectionPool: Database where Source.Connection: Database {
 
 extension MySQLError: DatabaseError { }
 
+private struct MySQLConverterDelegate: SQLConverterDelegate {
+    func customDataType(_ dataType: DatabaseSchema.DataType) -> SQLExpression? {
+        switch dataType {
+        case .string: return SQLRaw("VARCHAR(255)")
+        case .datetime: return SQLRaw("DATETIME(6)")
+        default: return nil
+        }
+    }
+    
+    func nestedFieldExpression(_ column: String, _ path: [String]) -> SQLExpression {
+        return SQLRaw("JSON_EXTRACT(\(column), '$.\(path[0])')")
+    }
+}
+
 extension MySQLConnection: Database {
     public func transaction<T>(_ closure: @escaping (Database) -> EventLoopFuture<T>) -> EventLoopFuture<T> {
         return closure(self)
     }
     
     public func execute(_ query: DatabaseQuery, _ onOutput: @escaping (DatabaseOutput) throws -> ()) -> EventLoopFuture<Void> {
-        let sql = SQLQueryConverter().convert(query)
-        return self.sqlQuery(sql) { row in
-            try onOutput(row.fluentOutput)
-        }.flatMapThrowing {
-            switch query.action {
-            case .create:
-                let row = LastInsertRow(okPacket: self.lastOKPacket!)
-                try onOutput(row)
-            default: break
-            }
-        }
+        let sql = SQLQueryConverter(delegate: MySQLConverterDelegate()).convert(query)
+        var serializer = SQLSerializer(dialect: MySQLDialect())
+        sql.serialize(to: &serializer)
+        return self.query(serializer.sql, serializer.binds.map { encodable in
+            return try! MySQLDataEncoder().encode(encodable)
+        }, onRow: { row in
+            try! onOutput(row.fluentOutput)
+        }, onMetadata: { metadata in
+            let row = LastInsertRow(metadata: metadata)
+            try! onOutput(row)
+        })
     }
     
     public func execute(_ schema: DatabaseSchema) -> EventLoopFuture<Void> {
-        return self.sqlQuery(SQLSchemaConverter().convert(schema)) { row in
+        return self.sqlQuery(SQLSchemaConverter(delegate: MySQLConverterDelegate()).convert(schema)) { row in
             fatalError("unexpected output")
         }
     }
@@ -82,21 +96,21 @@ extension MySQLConnection: Database {
 
 struct LastInsertRow: DatabaseOutput {
     var description: String {
-        return "\(self.okPacket)"
+        return "\(self.metadata)"
     }
     
-    let okPacket: MySQLProtocol.OK_Packet
+    let metadata: MySQLQueryMetadata
     
     func decode<T>(field: String, as type: T.Type) throws -> T where T : Decodable {
         #warning("TODO: fixme, better logic")
         switch field {
-        case "id":
+        case "fluentID":
             if T.self is Int.Type {
-                return Int(self.okPacket.lastInsertID!) as! T
+                return Int(self.metadata.lastInsertID!) as! T
             } else {
                 fatalError()
             }
-        default: fatalError()
+        default: throw ModelError.missingField(name: field)
         }
     }
 }
