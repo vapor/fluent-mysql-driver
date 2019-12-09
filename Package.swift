fdd025a520a5bf92bc7b2d6361a36739ee41501e
tanner0101 <me@tanner.xyz>
2019-12-09 15:20:41 -0500

    beta.2

--- Package.swift
+++ Package.swift
@@ -3,12 +3,15 @@ import PackageDescription
 
 let package = Package(
     name: "fluent-mysql-driver",
+    platforms: [
+       .macOS(.v10_14)
+    ],
     products: [
         .library(name: "FluentMySQLDriver", targets: ["FluentMySQLDriver"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/fluent-kit.git", .branch("master")),
-        .package(url: "https://github.com/vapor/mysql-kit.git", .branch("master")),
+        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/mysql-kit.git", from: "4.0.0-beta.2"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
