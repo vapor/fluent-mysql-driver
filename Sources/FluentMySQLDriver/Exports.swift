7a5a7155da758c2abb223d522002538449df83b1
Gwynne Raskind <gwynne@vapor.codes>
2023-05-26 14:43:46 -0500

    Update Exports.swift to correct usage
    No release needed; affects API docs deploy only.

--- Sources/FluentMySQLDriver/Exports.swift
+++ Sources/FluentMySQLDriver/Exports.swift
@@ -1,22 +1,22 @@
 #if swift(>=5.8)
 
-@_documentation(visiblity: internal) @_exported import FluentKit
+@_documentation(visibility: internal) @_exported import FluentKit
 
-@_documentation(visiblity: internal) @_exported import struct Foundation.URL
+@_documentation(visibility: internal) @_exported import struct Foundation.URL
 
-@_documentation(visiblity: internal) @_exported import struct MySQLKit.MySQLConfiguration
-@_documentation(visiblity: internal) @_exported import struct MySQLKit.MySQLConnectionSource
-@_documentation(visiblity: internal) @_exported import struct MySQLKit.MySQLDataEncoder
-@_documentation(visiblity: internal) @_exported import struct MySQLKit.MySQLDataDecoder
+@_documentation(visibility: internal) @_exported import struct MySQLKit.MySQLConfiguration
+@_documentation(visibility: internal) @_exported import struct MySQLKit.MySQLConnectionSource
+@_documentation(visibility: internal) @_exported import struct MySQLKit.MySQLDataEncoder
+@_documentation(visibility: internal) @_exported import struct MySQLKit.MySQLDataDecoder
 
-@_documentation(visiblity: internal) @_exported import class MySQLNIO.MySQLConnection
-@_documentation(visiblity: internal) @_exported import enum MySQLNIO.MySQLError
-@_documentation(visiblity: internal) @_exported import struct MySQLNIO.MySQLData
-@_documentation(visiblity: internal) @_exported import protocol MySQLNIO.MySQLDatabase
-@_documentation(visiblity: internal) @_exported import protocol MySQLNIO.MySQLDataConvertible
-@_documentation(visiblity: internal) @_exported import struct MySQLNIO.MySQLRow
+@_documentation(visibility: internal) @_exported import class MySQLNIO.MySQLConnection
+@_documentation(visibility: internal) @_exported import enum MySQLNIO.MySQLError
+@_documentation(visibility: internal) @_exported import struct MySQLNIO.MySQLData
+@_documentation(visibility: internal) @_exported import protocol MySQLNIO.MySQLDatabase
+@_documentation(visibility: internal) @_exported import protocol MySQLNIO.MySQLDataConvertible
+@_documentation(visibility: internal) @_exported import struct MySQLNIO.MySQLRow
 
-@_documentation(visiblity: internal) @_exported import struct NIOSSL.TLSConfiguration
+@_documentation(visibility: internal) @_exported import struct NIOSSL.TLSConfiguration
 
 #else
 
